ce754bd25d8f459bb8a7f70af466aa8f70846927
Markov Sergey <markov@qtech.ru>
2020-02-05 18:57:29 +0300

    Добавялет слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -9,12 +9,30 @@ $(document).ready(function(){
 
 // Swiper
 var swiper = new Swiper('.swiper-container', {
+  width: 1196,
   slidesPerView: 3,
   spaceBetween: 20,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  // breakpoints: {
+  //   // when window width is >= 768px
+  //   320: {
+  //     slidesPerView: 'auto',
+  //     spaceBetween: 20,
+  //   },
+  //   767: {
+  //     slidesPerView: 'auto',
+  //     spaceBetween: 20,
+  //   },
+  //   1023: {
+  //     slidesPerView: 'auto',
+  //     spaceBetween: 20,
+  //   }
+  // }
 });
+
+mySwiper.appendSlide('<div class="swiper-slide">Slide 10"</div>')
 
 
