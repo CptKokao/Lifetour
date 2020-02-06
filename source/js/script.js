d0926216bd3abdfeb13e82d2a46a146b7cd4eb93
Markov Sergey <markov@qtech.ru>
2020-02-06 18:01:48 +0300

    меняет блок tour

--- source/js/script.js
+++ source/js/script.js
@@ -1,38 +1,30 @@
-'use strict';
+"use strict";
 // hamburger
 
-$(document).ready(function(){
-  $(".hamburger").click(function(){
+$(document).ready(function() {
+  $(".hamburger").click(function() {
     $(this).toggleClass("is-active");
   });
 });
 
 // Swiper
-var swiper = new Swiper('.swiper-container', {
-  width: 1196,
-  slidesPerView: 3,
-  spaceBetween: 20,
+var swiper = new Swiper(".swiper-container", {
+
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
   },
-  // breakpoints: {
-  //   // when window width is >= 768px
-  //   320: {
-  //     slidesPerView: 'auto',
-  //     spaceBetween: 20,
-  //   },
-  //   767: {
-  //     slidesPerView: 'auto',
-  //     spaceBetween: 20,
-  //   },
-  //   1023: {
-  //     slidesPerView: 'auto',
-  //     spaceBetween: 20,
-  //   }
-  // }
+  breakpoints: {
+    // when window width is >= 768px
+    320: {
+      width: 'auto',
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    767: {
+      width: 1196,
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
 });
-
-mySwiper.appendSlide('<div class="swiper-slide">Slide 10"</div>')
-
-
